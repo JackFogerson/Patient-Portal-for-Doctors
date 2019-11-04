b787378a00f1328275d19f65a3afd1a87902ca8e
Nick Fulton <nickfulton98@gmail.com>
2019-11-04 14:22:18 -0800

    Changed layout of addNewPatientsPanel

--- src/com/patientlogger/AddNewPatientsPanel.java
+++ src/com/patientlogger/AddNewPatientsPanel.java
@@ -1,8 +1,5 @@
 package com.patientlogger;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 
@@ -21,7 +18,7 @@ public class AddNewPatientsPanel extends JPanel
 			
 	public AddNewPatientsPanel()
 	{
-		setLayout(new GridLayout(12, 4));
+		setLayout(new GridLayout(10, 4));
 		buildPanel();
 	}
 	
@@ -65,50 +62,54 @@ public class AddNewPatientsPanel extends JPanel
 		JLabel photoLabel = new JLabel("Photo");
 		JLabel ssnLabel = new JLabel("SSN");
 		JLabel insuranceLabel = new JLabel("Insurance");	
+		JLabel blankSpace1 = new JLabel();
+		JLabel blankSpace2 = new JLabel();
 		
 		JButton saveButton = new JButton("Save");
 		JButton addDemoButton = new JButton("Add Demographics");
 		JButton newVisitButton = new JButton("New Visit");
 		JButton cancelButton = new JButton("Cancel");	
 
-		add(THCNumberLabel, 0 , 0);
-		add(THCNumberField, 0, 1);
-		add(currentDateLabel, 0, 2);
-		add(currentDateField, 0, 3);
-		add(firstNameLabel, 1, 0);
-		add(firstNameField, 1, 1);
-		add(middleNameLabel, 1, 2);
-		add(middleNameField, 1, 3);
-		add(lastNameLabel, 2, 0);
-		add(lastNameField, 2, 1);
-		add(dobLabel, 2, 2);
-		add(dobField, 2, 3);
-		add(genderLabel, 3, 0);
-		add(genderField, 3, 1);
-		add(phoneLabel, 3, 2);
-		add(phoneField, 3, 3);
-		add(emailLabel, 4, 0);
-		add(emailField, 4, 1);
-		add(streetLabel, 4, 2);
-		add(streetField, 4, 3);
-		add(cityLabel, 5, 0);
-		add(cityField, 5, 1);
-		add(stateLabel, 5, 2);
-		add(stateField, 5, 3);
-		add(zipLabel, 6, 0);
-		add(zipField, 6, 1);
-		add(countryLabel, 6, 2);
-		add(countryField, 6, 3);
-		add(photoLabel, 7, 0);
-		add(photoField, 7, 1);
-		add(ssnLabel, 7, 2);
-		add(ssnField, 7, 3);
-		add(insuranceLabel, 8, 0);
-		add(insuranceField, 8, 1);
-		add(saveButton, 9, 0);
-		add(addDemoButton, 9, 1);
-		add(newVisitButton, 9, 2);
-		add(cancelButton, 9, 3);
+		add(THCNumberLabel);
+		add(THCNumberField);
+		add(currentDateLabel);
+		add(currentDateField);
+		add(firstNameLabel);
+		add(firstNameField);
+		add(middleNameLabel);
+		add(middleNameField);
+		add(lastNameLabel);
+		add(lastNameField);
+		add(dobLabel);
+		add(dobField);
+		add(genderLabel);
+		add(genderField);
+		add(phoneLabel);
+		add(phoneField);
+		add(emailLabel);
+		add(emailField);
+		add(streetLabel);
+		add(streetField);
+		add(cityLabel);
+		add(cityField);
+		add(stateLabel);
+		add(stateField);
+		add(zipLabel);
+		add(zipField);
+		add(countryLabel);
+		add(countryField);
+		add(photoLabel);
+		add(photoField);
+		add(ssnLabel);
+		add(ssnField);
+		add(insuranceLabel);
+		add(insuranceField);
+		add(blankSpace1);
+		add(blankSpace2);
+		add(saveButton);
+		add(addDemoButton);
+		add(newVisitButton);
+		add(cancelButton);
 				
 				
 	}
