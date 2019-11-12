c37a88731f7834b5449d36eaf4b608dd34c58c0d
Nick Fulton <nickfulton98@gmail.com>
2019-11-11 16:27:56 -0800

    Cleaned up the addnewpatients code a little bit

--- src/com/patientlogger/AddNewPatientsPanel.java
+++ src/com/patientlogger/AddNewPatientsPanel.java
@@ -1,8 +1,12 @@
 package com.patientlogger;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.io.File;
@@ -20,6 +24,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -54,22 +59,26 @@ public class AddNewPatientsPanel extends JPanel
 	
 	final String[] countryList = {"Select One", "United States of America"};
 	
-	final String[] monthList = {"--", "01", "02", "03", "04", "05", "06", "07",
-								"08", "09", "10", "11", "12"};
+	JFrame demographicsFrame;
 	
-	final String[] dayList = {"--", "01", "02", "03", "04", "05", "06", "07", "08", "09",
-							  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", 
-							  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
-							  "30", "31"};
+	Connection conn;	
 	
-	final String[] yearList = {"----", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012",
-							   "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003",
-							   "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994",
-							   "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985",
-							   "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976",
-							   "1975", "1974", "1973", "1972", "1971", "1970"};
+	JTextField THCNumberField, currentDateField, firstNameField, middleNameField, lastNameField, monthField,
+	 		   dayField, yearField, phoneField, emailField, streetField, zipField, ssnField, insuranceField,
+	 		   occupationField, workStatusField, educationField, tOnsetField, tEtioField, hOnsetField,
+	 		   hEtioField, commentField;
+	JComboBox<String> genderField, cityField, stateField, countryField;
 	
-	Connection conn;		
+	JButton photoField;
+	
+	JPanel dobField;
+	
+	JLabel THCNumberLabel, currentDateLabel, firstNameLabel, middleNameLabel, lastNameLabel, dobLabel,
+		   genderLabel, phoneLabel, emailLabel, streetLabel, cityLabel, stateLabel, zipLabel, countryLabel,
+		   photoLabel, ssnLabel, insuranceLabel, blankSpace1, blankSpace2, demographicsLabel, occupationLabel,
+		   workStatusLabel, educationLabel, tOnsetLabel, tEtioLabel, hOnsetLabel, hEtioLabel, commentLabel;
+	
+	JButton saveButton, addDemoButton, newVisitButton, cancelButton;
 	
 	public AddNewPatientsPanel(Connection c)
 	{
@@ -83,30 +92,50 @@ public class AddNewPatientsPanel extends JPanel
 		ImageIcon ogUnknownPicture = new ImageIcon("src/images/unknownPicture.png");
 		ImageIcon unknownPicture = new ImageIcon(ogUnknownPicture.getImage().getScaledInstance(40, 40, Image.SCALE_DEFAULT));
 		
-		JTextField THCNumberField = new JTextField();
-		JTextField currentDateField = new JTextField();
-		JTextField firstNameField = new JTextField();
-		JTextField middleNameField = new JTextField();
-		JTextField lastNameField = new JTextField();
-		JPanel dobField = new JPanel(new GridLayout(1, 3));
-		JTextField monthField = new JTextField("MM");
-		JTextField dayField = new JTextField("DD");
-		JTextField yearField = new JTextField("YYYY");
+		// Initalize the following for the normal form.
+		THCNumberField = new JTextField();
+		currentDateField = new JTextField();
+		firstNameField = new JTextField();
+		middleNameField = new JTextField();
+		lastNameField = new JTextField();
+		dobField = new JPanel(new GridLayout(1, 3));
+		monthField = new JTextField("MM");
+		dayField = new JTextField("DD");
+		yearField = new JTextField("YYYY");
 		dobField.add(monthField);
 		dobField.add(dayField);
 		dobField.add(yearField);
-		JComboBox<String> genderField = new JComboBox<String>(genderList);
-		JTextField phoneField = new JTextField();
-		JTextField emailField = new JTextField();
-		JTextField streetField = new JTextField();
-		JComboBox<String> cityField = new JComboBox<String>(cityList);
-		JComboBox<String> stateField = new JComboBox<String>(stateList);
-		JTextField zipField = new JTextField();
-		JComboBox<String> countryField = new JComboBox<String>(countryList);
-		JButton photoField = new JButton(unknownPicture);
-		JTextField ssnField = new JTextField();
-		JTextField insuranceField = new JTextField();
-
+		genderField = new JComboBox<String>(genderList);
+		phoneField = new JTextField();
+		emailField = new JTextField();
+		streetField = new JTextField();
+		cityField = new JComboBox<String>(cityList);
+		stateField = new JComboBox<String>(stateList);
+		zipField = new JTextField();
+		countryField = new JComboBox<String>(countryList);
+		photoField = new JButton(unknownPicture);
+		ssnField = new JTextField();
+		insuranceField = new JTextField();
+		
+		// Initialize the following for the demographics form.
+		occupationField = new JTextField();
+		workStatusField = new JTextField();
+		educationField = new JTextField();
+		tOnsetField = new JTextField();
+		tEtioField = new JTextField();
+		hOnsetField = new JTextField();
+		hEtioField = new JTextField();
+		commentField = new JTextField();
+		demographicsLabel = new JLabel("Demographics");
+		occupationLabel = new JLabel("Occupation");
+		workStatusLabel = new JLabel("Work Status");
+		educationLabel = new JLabel("Educational Degree");
+		tOnsetLabel = new JLabel("Tinnitus Onset");
+		tEtioLabel = new JLabel("Tinnitus Etiology");
+		hOnsetLabel = new JLabel("Hyperacusis Onset");
+		hEtioLabel = new JLabel("Hyperacusis Etiology");
+		commentLabel = new JLabel("Additional Comments");
+		
 		monthField.addFocusListener(new FocusListener() {
 
 			@Override
@@ -159,30 +188,30 @@ public class AddNewPatientsPanel extends JPanel
 		currentDateField.setText(dtf.format(localDate));
 		currentDateField.setBackground(Color.GRAY);
 		
-		JLabel THCNumberLabel = new JLabel("THC Number");
-		JLabel currentDateLabel = new JLabel("Current Date");
-		JLabel firstNameLabel = new JLabel("First Name");
-		JLabel middleNameLabel = new JLabel("Middle Name");
-		JLabel lastNameLabel = new JLabel("Last Name");
-		JLabel dobLabel = new JLabel("Date of Birth");
-		JLabel genderLabel = new JLabel("Gender");
-		JLabel phoneLabel = new JLabel("Phone");
-		JLabel emailLabel = new JLabel("Email");
-		JLabel streetLabel = new JLabel("Street");
-		JLabel cityLabel = new JLabel("City");
-		JLabel stateLabel = new JLabel("State");
-		JLabel zipLabel = new JLabel("Zipcode");
-		JLabel countryLabel = new JLabel("Country");
-		JLabel photoLabel = new JLabel("Photo");
-		JLabel ssnLabel = new JLabel("SSN");
-		JLabel insuranceLabel = new JLabel("Insurance");	
-		JLabel blankSpace1 = new JLabel();
-		JLabel blankSpace2 = new JLabel();
+		THCNumberLabel = new JLabel("THC Number");
+		currentDateLabel = new JLabel("Current Date");
+		firstNameLabel = new JLabel("First Name");
+		middleNameLabel = new JLabel("Middle Name");
+		lastNameLabel = new JLabel("Last Name");
+		dobLabel = new JLabel("Date of Birth");
+		genderLabel = new JLabel("Gender");
+		phoneLabel = new JLabel("Phone");
+		emailLabel = new JLabel("Email");
+		streetLabel = new JLabel("Street");
+		cityLabel = new JLabel("City");
+		stateLabel = new JLabel("State");
+		zipLabel = new JLabel("Zipcode");
+		countryLabel = new JLabel("Country");
+		photoLabel = new JLabel("Photo");
+		ssnLabel = new JLabel("SSN");
+		insuranceLabel = new JLabel("Insurance");	
+		blankSpace1 = new JLabel();
+		blankSpace2 = new JLabel();
 		
-		JButton saveButton = new JButton("Save");
-		JButton addDemoButton = new JButton("Add Demographics");
-		JButton newVisitButton = new JButton("New Visit");
-		JButton cancelButton = new JButton("Cancel");	
+		saveButton = new JButton("Save");
+		addDemoButton = new JButton("Add Demographics");
+		newVisitButton = new JButton("New Visit");
+		cancelButton = new JButton("Cancel");	
 		
 		saveButton.addActionListener(e -> submitInformation(THCNumberField, currentDateField, firstNameField,
 									 middleNameField, lastNameField, monthField, dayField, yearField, genderField, phoneField,
