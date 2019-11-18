419cc1b9dbbbd774b1c4edcdd45b4123ccbaeb53
Nick Fulton <nickfulton98@gmail.com>
2019-11-18 01:25:24 -0800

    Designed basic lookup patient, plan on adding a lot more information on the patient, probably all of the patient information in that screen.

--- src/com/patientlogger/LookupPatientPanel.java
+++ src/com/patientlogger/LookupPatientPanel.java
@@ -1,13 +1,17 @@
 package com.patientlogger;
 
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.sql.Connection;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class LookupPatientPanel extends JPanel
 {
@@ -32,19 +36,127 @@ public class LookupPatientPanel extends JPanel
 	
 	private void buildPanel()
 	{
+		ImageIcon ogUnknownPicture = new ImageIcon("src/images/unknownPicture.png");
+		ImageIcon unknownPicture = new ImageIcon(ogUnknownPicture.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+		
 		searchCriteria = new JComboBox<String>(searchOptions);
 		searchBox = new JTextField(10);
-		searchButton = new JButton();
+		searchButton = new JButton("Search");
 		
 		selectedPatientPanel = new JPanel();
-		selectedLabel = new JLabel("Selected Patient");
+		selectedLabel = new JLabel("Selected Patient", SwingConstants.CENTER);
+		selectedPatient = new Patient();
+		picture = new JLabel(unknownPicture);
+		name = new JLabel("Patient Name", SwingConstants.CENTER);
+		thc = new JLabel("THC Number", SwingConstants.CENTER);
+		ssn = new JLabel("SSN", SwingConstants.CENTER);
 		
-		getPatient();
+		searchButton.addActionListener(e -> search());
 		
+		selectedPatientPanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 50;
+		selectedPatientPanel.add(selectedLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 50;
+		selectedPatientPanel.add(picture, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 50;
+		selectedPatientPanel.add(thc, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 2;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 50;
+		selectedPatientPanel.add(name, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 3;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 50;
+		selectedPatientPanel.add(ssn, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 0;
+		add(searchCriteria, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 50;
+		add(searchBox, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 3;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 0;
+		add(searchButton, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.weighty = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.gridwidth = 4;
+		c.gridheight = 1;
+		c.ipady = 0;
+		c.ipadx = 0;
+		add(selectedPatientPanel, c);
 	}
 	
-	private void getPatient()
+	private void search()
 	{
 		
 	}
