358910be701c4a9e6741b22f23d3ade4714ffb4e
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-25 03:37:15 -0800

    Update Driver.java

--- src/com/patientlogger/Driver.java
+++ src/com/patientlogger/Driver.java
@@ -2,16 +2,14 @@ package com.patientlogger;
 
 /**
  * @title	Driver Class
- * @author	Nick Fulton
- * @desc	Is the starter class of this application.
- *
+ * @author	Nick Fulton, Jack Fogerson
+ * @desc	Starter class of application.
  */
 public class Driver 
 {
-	// Modification for the quiz.
 	public static void main(String[] args)
 	{
-		// Create the program and launch it.
+		// Creates program and launch it.
 		eTRT pl = new eTRT();
 		pl.launch();
 	}
