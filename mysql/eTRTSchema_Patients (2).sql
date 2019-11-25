75a48e87cf5d0254c41a3ed02cd9d686179df19f
jeevvansh <56938902+jeevvansh@users.noreply.github.com>
2019-11-24 17:43:11 -0800

    Update eTRTSchema_Patients (2).sql

--- mysql/eTRTSchema_Patients (2).sql
+++ mysql/eTRTSchema_Patients (2).sql
@@ -19,10 +19,10 @@
 -- Table structure for table `Patients`
 --
 
-/*DROP TABLE IF EXISTS `Patients`;
+DROP TABLE IF EXISTS `Patients`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-*/
+
 
 CREATE TABLE `Patients` (
   `THCNumber` int(11) NOT NULL,
