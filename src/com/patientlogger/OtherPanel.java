c4580d7c4fa3e945b408e3ed29798ad1d19c03a8
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-25 05:24:31 -0800

    Update OtherPanel.java

--- src/com/patientlogger/OtherPanel.java
+++ src/com/patientlogger/OtherPanel.java
@@ -2,14 +2,13 @@ package com.patientlogger;
 
 import java.awt.CardLayout;
 import java.sql.Connection;
-
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 /**
- * @title	PatientsPanel Class
- * @author 	Nick Fulton
- * @desc	Builds the patients panel.
+ * @title	OtherPanel Class
+ * @author 	Nick Fulton, Jack Fogerson
+ * @desc	Builds the Other panel.
  */
 public class OtherPanel extends JPanel
 {
@@ -19,7 +18,7 @@ public class OtherPanel extends JPanel
 	Connection conn;
 	
 	/**
-	 * @title	PatientsPanel Method
+	 * @title	OtherPanel Method
 	 * @param 	c - Connection to the database.
 	 * @desc	Sets the connection then builds the panel.
 	 */
@@ -31,19 +30,19 @@ public class OtherPanel extends JPanel
 	}
 	
 	/**
-	 * @title	buildPanel Method
-	 * @desc	Builds the patients panel.
+	 *	@title	buildPanel
+	 *	@desc	Local method used to build this panel, puts the components in the correct place.
 	 */
 	private void buildPanel()
 	{
 		// Create the tabbed pane.
 		mainPane = new JTabbedPane();
 		
-		// Create the needed panels.
+		// Create the panels within.
 		LocationDataPanel locationDataPanel = new LocationDataPanel(conn);
 		DemographicsDataPanel demographicsDataPanel = new DemographicsDataPanel(conn);
 		
-		// Add the panels to the pane.
+		// Add these panels to the pane.
 		mainPane.addTab("Location Data", null, locationDataPanel, "Add Location Data");
 		mainPane.addTab("Demographics Data", null, demographicsDataPanel, "Add Demographics Data");
 		
