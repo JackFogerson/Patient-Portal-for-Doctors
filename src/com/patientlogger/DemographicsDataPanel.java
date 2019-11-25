f60d9615b5e0d220aed48ea87b57f2eb4b61265a
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-25 03:32:16 -0800

    Added comments and @desc for clarification

--- src/com/patientlogger/DemographicsDataPanel.java
+++ src/com/patientlogger/DemographicsDataPanel.java
@@ -1,13 +1,23 @@
 package com.patientlogger;
 
 import java.sql.Connection;
-
 import javax.swing.JPanel;
 
+/**
+ * @title	AddNewVisitPanel
+ * @author	Nick Fulton, Jack Fogerson
+ * @desc	This class is an extension of JPanel
+ */
 public class DemographicsDataPanel extends JPanel
 {
+	private static final long serialVersionUID = 1L;
 	Connection conn;
 	
+	/**
+	 *	@title	DemographicsDataPanel
+	 *  @desc	constructor, builds panel
+	 *  @param 	c - Is the connection to the database.
+	 */
 	public DemographicsDataPanel(Connection c)
 	{
 		conn = c;
