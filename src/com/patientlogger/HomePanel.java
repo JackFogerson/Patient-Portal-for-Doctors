909bfc7250105c5deb9b988cbe5ee30b7c032c79
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-25 04:33:35 -0800

    Update pt. 1

--- src/com/patientlogger/HomePanel.java
+++ src/com/patientlogger/HomePanel.java
@@ -1,13 +1,35 @@
 package com.patientlogger;
 
+import javax.swing.GroupLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * @title	HomePanel
+ * @author	Nick Fulton, Jack Fogerson
+ * @desc	This class is an extension of JPanel. Serves as starting page on launch
+ */
 public class HomePanel extends JPanel
 {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	GroupLayout layout;
+
+	JLabel welcome;
 	
+	/**
+	 * @title	AddNewPatientsPanel
+	 * @desc	constructor, builds panel
+	 */
+	public HomePanel()
+	{
+		buildPanel();
+	}
+	
+	/**
+	 *	@title	buildPanel
+	 *	@desc	Local method used to build this panel, puts the components in the correct place.
+	 */
+	private void buildPanel()
+	{	
+	}
 }
